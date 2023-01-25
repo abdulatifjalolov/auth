45f4f47add3990ea5e39491cad52ad0e882eeb5d
Abdulkhafiz <abulhafizxalimjonov@gmail.com>
2023-01-25 16:25:08 +0500

    first change

--- src/main/java/org/example/DAO/BaseDao.java
+++ src/main/java/org/example/DAO/BaseDao.java
@@ -6,12 +6,12 @@ import org.hibernate.Session;
 import java.util.List;
 
 public interface BaseDao<T> {
-    public T getById(int id);
-    public void add(T item);
+    T getById(int id);
+    void add(T item);
 
-    public List<T> getList();
-    public void delete(T item);
-    default public void closeSession(Session session){
+    List<T> getList();
+    void delete(T item);
+    default void closeSession(Session session){
         session.getTransaction().commit();
         session.close();
     };
